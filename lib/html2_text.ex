ba77e799f8b91aab20dab581c5096f321bffb708
Artur Plysyuk <ifuelen@gmail.com>
2025-06-14 12:25:54 +0300

    Specify targets and nif versions

--- lib/html2_text.ex
+++ lib/html2_text.ex
@@ -16,10 +16,26 @@ defmodule HTML2Text do
   version = mix_config[:version]
   github_url = mix_config[:package][:links]["GitHub"]
 
+  targets = ~w(
+    aarch64-apple-darwin
+    aarch64-unknown-linux-gnu
+    aarch64-unknown-linux-musl
+    riscv64gc-unknown-linux-gnu
+    x86_64-apple-darwin
+    x86_64-pc-windows-gnu
+    x86_64-pc-windows-msvc
+    x86_64-unknown-linux-gnu
+    x86_64-unknown-linux-musl
+  )
+
+  nif_versions = ~w(2.17 2.16)
+
   use RustlerPrecompiled,
     otp_app: :html2text,
     crate: "html2text_nif",
     base_url: "#{github_url}/releases/download/v#{version}",
+    nif_versions: nif_versions,
+    targets: targets,
     version: version,
     force_build: System.get_env("HTML2TEXT_BUILD") in ["1", "true"]
 
